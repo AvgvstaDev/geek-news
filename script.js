7af2684ebda8ea5960c8ae683b7592ff745b4fa6
AvgvstaDev <vbarretocoelho@gmail.com>
2024-03-24 21:59:37 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var btnHam = document.querySelector(".ham-btn");
+var btnTimes = document.querySelector(".times-btn");
+var navBar = document.getElementById("nav-bar");
+
+btnHam.addEventListener('click', function(){
+    if(btnHam.className !== ""){
+        btnHam.style.display = "none";
+        btnTimes.style.display = "block";
+        navBar.classList.add("show-nav");
+    }
+})
+
+btnTimes.addEventListener('click', function(){
+    if(btnHam.className !== ""){
+        this.style.display = "none";
+        btnHam.style.display = "block";
+        navBar.classList.remove("show-nav");
+    }
+})
